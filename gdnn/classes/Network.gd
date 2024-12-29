5f73607c5355565dec6e8796fe2fb9d4be663e7b
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2024-12-30 08:02:52 +0900

    0006 ◎ C 12/30 12/30 0.001│ToDo│Network：コンストラクタで入力数を指定

--- gdnn/classes/Network.gd
+++ gdnn/classes/Network.gd
@@ -2,12 +2,11 @@
 class_name Network
 extends Node
 
+var m_nInput = 0
 
-# Called when the node enters the scene tree for the first time.
+func _init(nInput):
+	m_nInput = nInput	
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- gdnn/main.gd
+++ gdnn/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready():
-	var net = Network.new()
+	var net = Network.new(2)
 	pass # Replace with function body.
 func _process(delta):
 	pass
